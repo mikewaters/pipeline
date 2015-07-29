51149af14cb5dcf0fb7466be22c4bf3b2a4e42c1
Mike Waters <robert.waters@dealertrack.com>
2015-07-28 20:11:45 -0400

    Patchlevel bump.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='celery-pipeline',
-    version='0.9.3',
+    version='0.9.4',
     description="Runtime-configurable execution pipeline built on celery.",
     author='Mike Waters',
     author_email='robert.waters@gmail.com',
